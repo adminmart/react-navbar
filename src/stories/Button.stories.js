d05c507b1cc660592e7b206e0e9b8a509e0c29be
Ask Snbb <asksnbb@gmail.com>
2024-03-20 18:00:19 +0530

    new

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,14 +1,25 @@
 import { fn } from '@storybook/test';
 import { Button } from './Button';
-import { Header } from '../components/Header';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
   title: 'Example/Button',
-  component: Header,
-
+  component: Button,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn() },
 };
 
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary = {
   args: {
     primary: true,
@@ -16,4 +27,22 @@ export const Primary = {
   },
 };
 
+export const Secondary = {
+  args: {
+    label: 'Button',
+  },
+};
 
+export const Large = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+};
+
+export const Small = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
+};
